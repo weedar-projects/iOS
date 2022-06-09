fc7c1f32a5c0abd6a9c99a38ffffb22d12f195eb
kondratvladyslavdev <kondratievvladyslavdev@gmail.com>
2022-06-09 16:32:29 +0200

    add map view, radius picker

--- Weedar/Views/New/Tabs/Cart/PickUp/PickUpRootView.swift
+++ Weedar/Views/New/Tabs/Cart/PickUp/PickUpRootView.swift
@@ -10,34 +10,71 @@ import MapKit
 
 struct PickUpRootView: View {
     @State var selectedTab: PickUpShopListState = .list
+    @State var selectedLocation: Location?
+    @State var selectedRadius = 10
     
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State var showRaduisPicker = false
     
     var body: some View {
         ZStack{
+            
+            NavigationLink(isActive: $showRaduisPicker) {
+                GeolocationSelectView(selectedRadius: $selectedRadius)
+            } label: {
+                Color.clear
+            }
+
+            Color.col_white
+            
             VStack{
                 PickUpShopListPiker(selected: $selectedTab)
                 
-                Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                    MapAnnotation(coordinate: location.coordinate) {
-                        Circle()
-                            .stroke(.red, lineWidth: 3)
-                            .frame(width: 44, height: 44)
+    
+                RadiusSelectBtn()
+                    .padding(.top, 24)
+                    .onTapGesture {
+                        showRaduisPicker = true
                     }
-                }
-                .vTop()
+                Text("Stores available: 4")
+                    .textSecond()
+                    .padding([.horizontal,.top], 24)
+                    .hLeading()
+                
+                PickUpMapView(selectedLocation: $selectedLocation)
+                    
                 
                 Spacer()
-                
             }
         }
         .navBarSettings("Choose store")
+    }
+    
+    
+    @ViewBuilder
+    func RadiusSelectBtn() -> some View{
+        
+        HStack(spacing: 0){
+            Image("mapMarker_icon")
+                .scaleEffect(0.8)
+                .padding(.leading , 16)
+            
+            Text("Within \(selectedRadius) miles")
+                .textCustom(.coreSansC45Regular, 16, Color.col_text_main)
+            Spacer()
+            Image("arrowRight")
+                .padding(.trailing , 16)
+        }
+        .padding(.horizontal, 24)
+        .frame(height: 48)
+        .background(Color.white)
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+            .strokeBorder()
+            .frame(height: 48)
+            .foregroundColor(Color.col_gray_main)
+            .padding(.horizontal, 24)
+        )
+        
     }
 }
 
@@ -48,7 +85,7 @@ struct PickUpRootView_Previews: PreviewProvider {
 }
 
 struct Location: Identifiable {
-    let id = UUID()
+    let id: Int
     let name: String
     let coordinate: CLLocationCoordinate2D
 }
@@ -59,15 +96,43 @@ enum PickUpShopListState{
 }
 
 
+struct PickUpMapView: View {
+    @Binding var selectedLocation: Location?
+    @State var locations = [
+        Location(id: 1, name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(id: 2, name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    var body: some View{
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+            MapAnnotation(coordinate: location.coordinate) {
+                ZStack{
+                    Image(location.id == selectedLocation?.id ?? 0 ? "selected_mapMarker_icon" : "mapMarker_icon")
+                        .offset(y: location.id == selectedLocation?.id ?? 0 ? -22 : -11)
+                        .onTapGesture {
+                            self.selectedLocation = location
+                        }
+                }
+            }
+        }
+        .frame(height: 338)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .padding(.horizontal, 24)
+        
+    }
+}
+
 struct PickUpShopListPiker: View {
     
     @Binding var selected: PickUpShopListState
-    private 
+    
+    
     var body: some View {
             HStack(spacing: 0){
                 Text("See list")
                     .textCustom(.coreSansC65Bold, 16, selected == .list ? Color.col_text_white : Color.col_text_main)
-                    .frame(width: (width / 2) - 24, height: 44)
+                    .frame(width: (getRect().width / 2 - 24), height: 44)
                     .background(Color.col_white.opacity(0.01))
                     .onTapGesture {
                         selected = .list
@@ -75,7 +140,7 @@ struct PickUpShopListPiker: View {
                 
                 Text("See on map")
                     .textCustom(.coreSansC65Bold, 16, selected == .map ? Color.col_text_white : Color.col_text_main)
-                    .frame(width: (width / 2) - 24, height: 44)
+                    .frame(width: (getRect().width / 2 - 24), height: 44)
                     .background(Color.col_white.opacity(0.01))
                     .onTapGesture {
                         selected = .map
@@ -86,13 +151,13 @@ struct PickUpShopListPiker: View {
             .background(
                 HStack{
                     Color.col_black
-                        .frame(width: (width / 2) - 24, height: 44)
+                        .frame(width: (getRect().width / 2 - 24), height: 44)
                         .cornerRadius(radius: 12, corners: selected == .map ? [.bottomRight, .topRight] : [.bottomLeft, .topLeft])
                         .animation(.interactiveSpring(response: 0.15,
                                                       dampingFraction: 0.65,
                                                       blendDuration: 1.5),
                                    value: selected)
-                        .offset(x: selected == .map ? (width / 2) - 24 : 0)
+                        .offset(x: selected == .map ? (getRect().width / 2 - 24) : 0)
                         .hLeading()
                 }
             )
@@ -103,7 +168,68 @@ struct PickUpShopListPiker: View {
                 
             )
             .padding(.horizontal, 24)
-            
-        }
+        
     }
 }
+
+
+
+struct GeolocationSelectView: View {
+    @Binding var selectedRadius: Int
+    
+    var radiusValues = [5, 10, 15, 25]
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            ForEach(radiusValues, id: \.self) { value in
+                HStack{
+                    Image("location_icon")
+                        .opacity(selectedRadius == value ? 1 : 0.22)
+                        
+                    Text("Within \(value) miles")
+                        .textCustom(selectedRadius == value ? .coreSansC55Medium : .coreSansC45Regular, 16, Color.col_text_main)
+                    
+                    Spacer()
+                    
+                    ZStack{
+                        RadialGradient(colors: [Color.col_gradient_green_second,
+                                                Color.col_gradient_green_first],
+                                       center: .center,
+                                       startRadius: 0,
+                                       endRadius: 25)
+                        Image("checkmark")
+                                    .resizable()
+                                    .foregroundColor(Color.col_black)
+                                    .scaledToFit()
+                                    .frame(width: 14, height: 14)
+                    }
+                    .clipShape(Circle())
+                    .frame(width: 28, height: 28)
+                    .opacity(selectedRadius == value ? 1 : 0)
+                }
+                .frame(height: 50)
+                .background(Color.col_white)
+                .onTapGesture {
+                    selectedRadius = value
+                }
+                
+                CustomDivider()
+                    .opacity(value == radiusValues.last ? 0 : 1)
+            }
+            
+            Spacer()
+            
+            MainButton(title: "Comfirm") {
+                
+            }
+        }
+        .padding(.horizontal, 24)
+        .navBarSettings("Geolocation")
+    }
+}
+
+//struct GeolocationSelectView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        GeolocationSelectView()
+//    }
+//}
